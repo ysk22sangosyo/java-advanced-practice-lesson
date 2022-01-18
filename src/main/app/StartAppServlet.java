bfba6837ad51c5ba4abba2f38a06adc8e5e0e030
KinjoAkari <a.kinjo@tc-tech.co.jp>
2022-01-18 11:57:47 +0900

    演習問題3 基本 ・発展

--- src/main/app/StartAppServlet.java
+++ src/main/app/StartAppServlet.java
@@ -38,34 +38,6 @@ public class StartAppServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        /*
-           todoコメントを参考に、必要な処理を実装してください。
-           ※記載済の処理は消さないでください。
-         */
-
-        // 入力値取得
-        request.setCharacterEncoding("UTF-8");
-        String name = request.getParameter("name");
-
-        // todo:ラジオボタンの選択値を取得
-
-        if (name != null && !name.isEmpty()) {
-            GameApp app;
-
-            // todo:
-            // ラジオボタンの選択値に応じて、下記の通りオブジェクトを作成する
-            // トランプを選択した場合：CardGameAppのオブジェクトを作成し、変数appに代入
-            // ゲームを選択した場合：GameAppのオブジェクトを作成し、変数appに代入
-
-            // アプリ実行結果のメッセージを入れるための変数
-            String result = "";
-
-            // todo:作成したオブジェクトのstartメソッドを呼び、戻り値を変数resultへ代入する
-
-            // リクエストスコープへセット
-            request.setAttribute("result", result);
-        }
-
-        request.getRequestDispatcher("appStart.jsp").forward(request, response);
+    	// ここに必要な処理を実装してください。
     }
 }
